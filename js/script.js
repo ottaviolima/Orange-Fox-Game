9ec1208b16778ea7db7761bae82b4b8306444eec
Otávio Lima <otavio-lima-co@gmail.com>
2024-02-07 18:42:44 -0300

    Add Score

--- js/script.js
+++ js/script.js
@@ -1,41 +1,51 @@
-document.addEventListener('keydown', jump)
+let score = 0; 
+
+function startCounting() {
+    const scoreDisplay = document.getElementById('score');
+    scoreInterval = setInterval(() => {
+        score++;
+        scoreDisplay.textContent = score;
+    }, 1000); 
+}
 
 function jump() {
     const fox = document.getElementById('fox-running');
     fox.className = "jump"
     fox.src = 'images/fox-jumping.gif'
-    
 
     setTimeout(() => {
         fox.className = "fox"
         fox.src = 'images/fox-running.gif'
-    },500)
-}  
-
-const loop = setInterval(() => {
-
-    const tree = document.getElementById('tree');
-    const fox = document.getElementById('fox-running');
-    const gameOver = document.getElementById('game-over');
-    const reset = document.getElementById('reset');
-    const treePosition = tree.offsetLeft;
-    const foxPosition = +window.getComputedStyle(fox).bottom.replace('px', '');
-
-    if (treePosition < 120 && treePosition > 0 && foxPosition < 80) {
-
-        tree.style.animation = 'none'
-        tree.style.left = `${treePosition}px`
-
-        fox.style.animation = 'none'
-        fox.style.bottom = `${foxPosition}px`
-        gameOver.className = "game-over-visible"
-        reset.className = "reset-visible"
-
-        clearInterval(loop)
-    }
-
-},10)
+    }, 500)
+}
 
 function reset() {
-    location.reload()
+    location.reload();
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    startCounting();
+
+    document.addEventListener('keydown', jump);
+
+    loop = setInterval(() => {
+        const tree = document.getElementById('tree');
+        const fox = document.getElementById('fox-running');
+        const gameOver = document.getElementById('game-over');
+        const resetButton = document.getElementById('reset');
+        const treePosition = tree.offsetLeft;
+        const foxPosition = +window.getComputedStyle(fox).bottom.replace('px', '');
+
+        if (treePosition < 120 && treePosition > 0 && foxPosition < 80) {
+            tree.style.animation = 'none';
+            tree.style.left = `${treePosition}px`;
+            fox.style.animation = 'none';
+            fox.style.bottom = `${foxPosition}px`;
+            gameOver.className = "game-over-visible";
+            resetButton.className = "reset-visible";
+
+            clearInterval(loop);
+            clearInterval(scoreInterval); // Parando a contagem do score
+        }
+    }, 10);
+});
